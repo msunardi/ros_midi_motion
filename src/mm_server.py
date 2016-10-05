a714392c66fea2bdfae9208f5487be9c69990f57
Mathias@banana <m.sunardi@gmail.com>
2016-10-04 23:16:22 -0700

    Add all files

---
+++ src/mm_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from midi_motion.srv import *
+import rospy, rospkg
+
+import numpy as np
+from readmidi import Readmidi
+import analyzer as az
+
+def extract(service_request):
+	# rospy.loginfo(filename)
+	# if not filename:
+	# 	return MidiInfoResponse("fubar" ,"456")
+	# return MidiInfoResponse("barfoo", "123")
+	path = service_request.source
+	if not path:
+		return False
+
+	print "Extract: %s" % path
+
+	r = Readmidi()
+	rm = r.openfile(path)
+	events = r.parseevents(ret=2)
+	nopb = az.analyzeNoteOnPerBeat(events)
+	t, m = az.analyzePhrase3(nopb)
+	return MidiInfoResponse(str(t), str(m))
+	# return MidiInfoResponse("foo", "bar")
+
+
+def mm_server():
+	rospy.init_node('midi_motion_server')
+	s = rospy.Service('midi_motion_server', MidiInfo, extract)
+	print "Ready to extract MIDI"
+	rospy.spin()
+
+if __name__ == "__main__":
+    mm_server()

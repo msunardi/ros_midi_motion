301bbe5fc174e159f7b15463d6a3d78712cad36a
Mathias Sunardi <misunardi@hotmail.com>
2017-10-27 12:25:27 -0700

    Change tabs to spaces

--- src/mm_client.py
+++ src/mm_client.py
@@ -7,26 +7,26 @@ from midi_motion.srv import *
 rospack = rospkg.RosPack()
 
 def mm_client(filename):
-	rospy.wait_for_service('midi_motion_server')
-	try:
-		mm = rospy.ServiceProxy('midi_motion_server', MidiInfo)
-		r = mm(filename)
-		return r.events, r.timing
-	except rospy.ServiceException, e:
-		print "Service call failed: %s" % e
+    rospy.wait_for_service('midi_motion_server')
+    try:
+        mm = rospy.ServiceProxy('midi_motion_server', MidiInfo)
+        r = mm(filename)
+        return r.events, r.timing
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
 
 def usage():
-	return "Either give a path to .med file or a default will be given."
+    return "Either give a path to .med file or a default will be given."
 
 if __name__ == "__main__":
-	filename = None
-	if len(sys.argv) == 2:
-		filename = str(sys.argv[1])
-	else:
-		print usage()
-		filename = "lullaby_of_birdland.med"
-	path = '%s/src/%s' % (rospack.get_path('midi_motion'), filename)
-	print path
-	print "Requesting MIDI information ... %s" % path
-	timing, accent = mm_client(path)
-	print timing
+    filename = None
+    if len(sys.argv) == 2:
+        filename = str(sys.argv[1])
+    else:
+        print usage()
+        filename = "lullaby_of_birdland.med"
+    path = '%s/src/%s' % (rospack.get_path('midi_motion'), filename)
+    print path
+    print "Requesting MIDI information ... %s" % path
+    timing, accent = mm_client(path)
+    print timing
--- src/mm_server.py
+++ src/mm_server.py
@@ -8,30 +8,30 @@ from readmidi import Readmidi
 import analyzer as az
 
 def extract(service_request):
-	# rospy.loginfo(filename)
-	# if not filename:
-	# 	return MidiInfoResponse("fubar" ,"456")
-	# return MidiInfoResponse("barfoo", "123")
-	path = service_request.source
-	if not path:
-		return False
+    # rospy.loginfo(filename)
+    # if not filename:
+    # 	return MidiInfoResponse("fubar" ,"456")
+    # return MidiInfoResponse("barfoo", "123")
+    path = service_request.source
+    if not path:
+        return False
 
-	print "Extract: %s" % path
+    print "Extract: %s" % path
 
-	r = Readmidi()
-	rm = r.openfile(path)
-	events = r.parseevents(ret=2)
-	nopb = az.analyzeNoteOnPerBeat(events)
-	t, m = az.analyzePhrase3(nopb)
-	return MidiInfoResponse(str(t), str(m))
-	# return MidiInfoResponse("foo", "bar")
+    r = Readmidi()
+    rm = r.openfile(path)
+    events = r.parseevents(ret=2)
+    nopb = az.analyzeNoteOnPerBeat(events)
+    t, m = az.analyzePhrase3(nopb)
+    return MidiInfoResponse(str(t), str(m))
+    # return MidiInfoResponse("foo", "bar")
 
 
 def mm_server():
-	rospy.init_node('midi_motion_server')
-	s = rospy.Service('midi_motion_server', MidiInfo, extract)
-	print "Ready to extract MIDI"
-	rospy.spin()
+    rospy.init_node('midi_motion_server')
+    s = rospy.Service('midi_motion_server', MidiInfo, extract)
+    print "Ready to extract MIDI"
+    rospy.spin()
 
 if __name__ == "__main__":
     mm_server()
